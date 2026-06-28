ef1b6acf1b12df5154774c9eb00440efbc6b8c65
Juan Garduno <juan.garduno@mlb.com>
2026-06-28 08:30:10 -0600

    Will this be it?

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      fetchCache: 'force-no-store',
-    }
-  };
-  export default nextConfig;
+  headers: async () => [
+    {
+      source: '/api/:path*',
+      headers: [
+        { key: 'Cache-Control', value: 'no-store, max-age=0' },
+        { key: 'CDN-Cache-Control', value: 'no-store' },
+        { key: 'Vercel-CDN-Cache-Control', value: 'no-store' },
+      ],
+    },
+  ],
+}
+export default nextConfig;
